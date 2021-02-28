f54244ee71e75111fae63d163eb91cd495cb2610
jaime <jaime@jaimes-MacBook.local>
2021-02-27 21:50:31 -0700

    worked on js and fetching API

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,2 +1,17 @@
-var mykey = config.MY_KEY;
+const mykey = config.MY_KEY;
+const url = 'https://api.openweathermap.org/data/2.5/forecast?q='
 
+
+$(document).ready(function () {
+  $(".btn").click(function (event) {
+    event.preventDefault();
+    var cityName = $("#search").val()
+    $("#search").val("")
+    // getWeather(cityName)
+    console.log(cityName)
+    fetch(url + cityName + '&appid=' + mykey + "&units=imperial",)
+      .then(function (response) {
+        return response.json();
+      })
+  });
+});
