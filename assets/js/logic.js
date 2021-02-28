a4e3eb2d77836bc77689de173031e0feb27b6bd3
jaime <jaime@jaimes-MacBook.local>
2021-02-28 16:26:14 -0700

    got icon img to fetch

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,20 +1,39 @@
 const mykey = 'e41cae56b33edbfc24408b3b37caf9e3';
 const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=';
 const todayUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const iconUrl = "https://api.openweathermap.org/data/2.5/img/w/"
+var iconContainerEl = document.querySelector('.city-info')
 const cities = [];
+
+
+// //Getting local storage if any
+// if (localStorage.getItem("cities") !== null) {
+//   cities = JSON.parse(localStorage.getItem("cities"));
+//   for (var i = 0; i < cities.length; i++) {
+//     var cityListadd = $("<div>").addClass("city");
+//     cityListadd.text(cities[i]);
+//     $("#city-history").append(cityListadd);
+//   }
+// }
 
 function createBtns() {
   $('#city-history').empty();
   cities.forEach(function (item) {
     $('#city-history').append(`
-   <button class="btn mt-2 btn-outline-secondary btn-block">${item}</button>
-   `)
-  })
+    <button class="btn-2 mt-2 btn-outline-secondary btn-block">${item}</button>
+    `)
+    // $(".btn-2").click((getForecast))
+    populateHistory();
+  });
 }
+
+
+
 
 $(document).ready(function () {
   $(".btn").click(function (event) {
     event.preventDefault();
+    // $('.hide').show();
     var cityName = $("#search").val()
     $("#search").val("")
     getWeather(cityName)
@@ -24,7 +43,7 @@ $(document).ready(function () {
     createBtns();
   });
 });
-
+// 5days
 function getForecast(searchvalue) {
   fetch(forecastUrl + searchvalue + '&appid=' + mykey + "&units=imperial",)
     .then(function (response) {
@@ -33,10 +52,12 @@ function getForecast(searchvalue) {
       console.log(data)
       for (var i = 0; i < data.list.length; i += 8) {
 
+        var todImg = data.list[i].weather[0].icon
+        $(".today-img").attr("src", `https://openweathermap.org/img/wn/${todImg}@2x.png`)
       }
     })
 }
-
+// today
 function getWeather(searchvalue) {
   fetch(todayUrl + searchvalue + '&appid=' + mykey + "&units=imperial",)
     .then(function (response) {
@@ -44,6 +65,14 @@ function getWeather(searchvalue) {
     }).then(function (data) {
       console.log(data)
       getUvi(data.coord.lat, data.coord.lon);
+      cities.push(searchvalue)
+      $(".city").text(searchvalue + " " + moment().format("l"))
+
+      $(".temp").text("Temperature: " + data.main.temp + "° F")
+      $(".humidity").text("Humidity: " + data.main.humidity + "%")
+      $(".wind").text("Wind Speed: " + data.wind.speed + " mph")
+      // $(".UV").text(getUvi);
+
     })
 }
 
@@ -54,5 +83,10 @@ function getUvi(lat, long) {
     }).then(function (data) {
       console.log(data)
     });
+}
+
+populateHistory = (city) => {
+  cities.push(city);
+  localStorage.setItem("cities", JSON.stringify(cities));
 }
 
