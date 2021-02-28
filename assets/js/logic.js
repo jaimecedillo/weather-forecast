3fae522f0e0aae207aa29dff2358c068408e1753
jaime <jaime@jaimes-MacBook.local>
2021-02-28 10:56:20 -0700

    functions to call info needed

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,17 +1,58 @@
-const mykey = config.MY_KEY;
-const url = 'https://api.openweathermap.org/data/2.5/forecast?q='
+const mykey = 'e41cae56b33edbfc24408b3b37caf9e3';
+const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+const todayUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const cities = [];
 
+function createBtns() {
+  $('#city-history').empty();
+  cities.forEach(function (item) {
+    $('#city-history').append(`
+   <button class="btn mt-2 btn-primary btn-outline-secondary">${item}</button>
+   `)
+  })
+}
 
 $(document).ready(function () {
   $(".btn").click(function (event) {
     event.preventDefault();
     var cityName = $("#search").val()
     $("#search").val("")
-    // getWeather(cityName)
+    getWeather(cityName)
     console.log(cityName)
-    fetch(url + cityName + '&appid=' + mykey + "&units=imperial",)
-      .then(function (response) {
-        return response.json();
-      })
+    getForecast(cityName);
+    cities.push(cityName)
+    createBtns();
   });
 });
+
+function getForecast(searchvalue) {
+  fetch(forecastUrl + searchvalue + '&appid=' + mykey + "&units=imperial",)
+    .then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      console.log(data)
+      for (var i = 0; i < data.list.length; i += 8) {
+
+      }
+    })
+}
+
+function getWeather(searchvalue) {
+  fetch(todayUrl + searchvalue + '&appid=' + mykey + "&units=imperial",)
+    .then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      console.log(data)
+      getUvi(data.coord.lat, data.coord.lon);
+    })
+}
+
+function getUvi(lat, long) {
+  fetch(`http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${long}&appid=${mykey}`,)
+    .then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      console.log(data)
+    });
+}
+
